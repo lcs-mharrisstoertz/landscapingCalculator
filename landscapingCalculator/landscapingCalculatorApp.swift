13194c869d80a288ad030d06f96346058481dfda
Morgan <Mharrisstoertz@gmail.com>
2023-02-07 12:22:01 -0500

    made new app entry point

--- landscapingCalculator/landscapingCalculatorApp.swift
+++ landscapingCalculator/landscapingCalculatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct landscapingCalculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            listView()
         }
     }
 }
---
+++ landscapingCalculator/views/listView.swift
@@ -0,0 +1,20 @@
+//
+//  listView.swift
+//  landscapingCalculator
+//
+//  Created by Morgan Harris-Stoertz on 2023-02-07.
+//
+
+import SwiftUI
+
+struct listView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct listView_Previews: PreviewProvider {
+    static var previews: some View {
+        listView()
+    }
+}
