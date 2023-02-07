62e8192656110c60afe6b1a993609da005c55bd4
Morgan <Mharrisstoertz@gmail.com>
2023-02-07 11:01:03 -0500

    Initial Commit

---
+++ landscapingCalculator/landscapingCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  landscapingCalculatorApp.swift
+//  landscapingCalculator
+//
+//  Created by Morgan Harris-Stoertz on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct landscapingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
